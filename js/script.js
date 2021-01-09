8ddb96caa2e2a5fb6f6d2d75ccb26366014cb971
namanjo <naman14joshi@gmail.com>
2021-01-09 23:17:15 +0530

    added responsiveness to the website
    Created the Home page as well as the recruiter's view. Added media queries to make the website responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+  $('.js--nav-icon').click(function() {
+    var nav = $('.my-nav');
+    var icon = $('.my-nav i');
 
-  
 
+    nav.slideToggle(400);
+
+    if(icon.hasClass('ion-navicon-round')){
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else{
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+
+  });
 });
